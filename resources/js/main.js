511ce44252e215e291640359750b983cd4d81322
lawdy33 <lawdy33@gmail.com>
2017-06-22 18:09:01 -0700

    nearly done added all sections

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.jumbotron').slick( {
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
