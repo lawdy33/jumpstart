6149e5493148145ffc5d55ca33f1dd2b7d319eb2
lawdy33 <lawdy33@gmail.com>
2017-06-23 15:37:37 -0700

    completed project

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,8 @@
+
+// Slick - Carousel - jumbotron photos //
+
 $(document).ready(function () {
+
   $('.jumbotron').slick( {
     arrows: false,
     autoplay: true,
@@ -6,5 +10,22 @@ $(document).ready(function () {
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
+  });
+
+// hide navbar //
+
+  var scrollMovement = 0;
+
+  $(window).scroll(function (){
+    var scrollAction = $(this).scrollTop();
+
+    if (scrollAction - scrollMovement > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: "-" + navHeight}, 150);
+      scrollMovement = scrollAction;
+    } else if (scrollMovement - scrollAction > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollMovement = scrollAction;
+    }
   });
 });
